e5d48f6acb3a172574fe06f29b91be11138df677
Cheng Adrian <chengadrian@ChengdeMacBook-Pro.local>
2023-04-25 21:44:02 +0100

    session

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const port = process.env.PORT || 2050;
 
 const db = new sqlite3.Database('./db/data.db')
 
-db.exec('create table if not exists account (id integer unique not null primary key autoincrement, user varchar(40) not null unique, pass varchar(40) not null)')
+db.exec(`create table if not exists account (
+    id integer unique not null primary key autoincrement,
+    user varchar(40) not null unique,
+    pass varchar(40) not null)`)
+
+db.exec(`create table if not exists session (
+    id integer not null unique,
+    session varchar(12) not null unique,
+    foreign key (id) references account (id))`)
 
 // db.exec(`insert into account (user, pass) values ('a', '1')`)
 
@@ -40,6 +48,75 @@ const searchAll = (table, data, cb) => {
     })
 }
 
+function getSession(user, pass, res, ses) {
+    if (!ses) {
+        // console.log('normal case')
+        let session = Math.round(Math.random()*3)+1;
+        searchAll('session', {session: session}, (rows)=>{
+            if (rows.length >= 1) {
+                getSession(user, pass, res, ses);
+                return
+            }
+            else {
+                // console.log(user);
+                searchAll('account', {user: user}, (r)=>{
+                    // console.log(r)
+                    let row = r[0];
+                    let id = row.id;
+                    searchAll('session', {id: id}, (rs)=>{
+                        if (rs.length >= 1) {
+                            db.run('update session set session=? where id=?', [session, id], (err)=>{
+                                if (err) throw err;
+                                res.send({success: true, session: session})
+                                // if (ses) {
+                                //     console.log(`Login - (session: ${ses})`)
+                                // }
+                                // else {
+                                    console.log(`Login - (user: ${user}, pass: ${pass})`.green)
+                                // }
+                                return
+                            })
+                        }
+                        else {
+                            insertRow('session', {id: id, session: session}, ()=>{
+                                res.send({success: true, session: session})
+                                // if (session) {
+                                //     console.log(`Login - (session: ${ses})`)
+                                // }
+                                // else {
+                                    console.log(`Login - (user: ${user}, pass: ${pass})`.green)
+                                // }
+                                return
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    }
+    else {
+        // console.log('session case');
+        let session = Math.round(Math.random()*3+1);
+        searchAll('session', {session: session}, (rows)=>{
+            if (rows.length >= 1) {
+                getSession(user, pass, res, ses);
+                return;
+            }
+            else {
+                searchAll('session', {session: ses}, (r)=>{
+                    let id = r[0].id;
+                    db.run('update session set session=? where id=?', [session, id], (err)=>{
+                        if (err) throw err;
+                        res.send(JSON.stringify({success: true, session: session}));
+                        console.log(`Login - (session: ${ses})`.green)
+                        return;
+                    })
+                })
+            }
+        })
+    }
+}
+
 /*
 Error codes:
 0: Empty user or pass
@@ -49,6 +126,22 @@ Error codes:
 app.post('/login',(req, res)=>{
     let body = req.body;
 
+    if (body.session) {
+        // console.log('session in post')
+        searchAll('session', {session: body.session}, (rows)=>{
+            if (rows.length >= 1) {
+                // console.log('inside')
+                getSession(null, null, res, body.session)
+                return;
+            }
+            else {
+                res.send(JSON.stringify({success: false, reason: 4}));
+                return;
+            }
+        })
+        return;
+    }
+
     if (!(body.user && body.pass)) {
         res.send({success: false, reason: 0})
         return;
@@ -56,9 +149,10 @@ app.post('/login',(req, res)=>{
 
     searchAll('account', {user: body.user, pass: body.pass}, (rows)=>{
         if (rows.length >= 1) {
-            res.send({success: true})
-            console.log(`Login - (user: ${body.user}, pass: ${body.pass}).green`)
-            return
+            // res.send({success: true, session: session})
+            // console.log(`Login - (user: ${body.user}, pass: ${body.pass}).green`)
+            // return
+            getSession(body.user, body.pass, res, null);
         }
         else {
             res.send({success: false, reason: 1})
